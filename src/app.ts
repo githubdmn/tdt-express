a0b6e5951da9e926c3e13c5eb072cbcc0eb7cb64
Damjan Ivanovic <damjanivanovic@Damjans-MacBook-Pro.local>
2021-12-08 15:17:57 +0100

    express init

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,19 @@
 
 import dev from './config'
 import { mongoConnect } from './database'
-import { express } from 'express'
-
-console.log(dev())
+import express, {Request, Response} from 'express'
 
 mongoConnect()
   .then(() => console.log('successfully connected to mongodb'))
   .catch(() => console.log('unsuccessfully connected to mongodb'))
+
+const indexHeader = `<h3>21-12<h3></break>`;
+const indexMessage = `<p>Date posted 08/12/2021\nHome index - ${new Date()}</p>`;
+
+express()
+	.use(express.json())
+	.use(express.urlencoded({ extended: true }))
+	.get('/', (req: Request, res:Response) => { res.status(200).send(indexHeader.concat(indexMessage)) })
+	.get('/health', (req: Request, res:Response) => { res.status(200).send('OK') })
+	.listen(() => console.log(`Listening on port ${dev().port}...`));
+
