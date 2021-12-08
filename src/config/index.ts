a49293d0729eb50692a1e9faff85658f220924b8
Damjan Ivanovic <damjanivanovic@Damjans-MacBook-Pro.local>
2021-12-08 14:15:38 +0100

    environment values as function

--- src/config/index.ts
+++ src/config/index.ts
@@ -7,15 +7,17 @@ config({
 	path: resolve(__dirname, '../../.env'),
 });
 
-const dev = {
-	port: env.PORT,
-	salt: env.SALT_WORK_FACTOR,
-	access_token_secret: env.ACCESS_TOKEN_SECRET,
-	refresh_token_secret: env.REFRESH_TOKEN_SECRET,
-	mongo_username: env.MONGO_USERNAME,
-	mongo_password: env.MONGO_PASSWORD,
-	mongo_server: env.MONGO_SERVER,
-	mongo_name: env.MONGO_NAME,
+const dev = () => {
+	return {
+		port: env.PORT,
+		salt: env.SALT_WORK_FACTOR,
+		access_token_secret: env.ACCESS_TOKEN_SECRET,
+		refresh_token_secret: env.REFRESH_TOKEN_SECRET,
+		mongo_username: env.MONGO_USERNAME,
+		mongo_password: env.MONGO_PASSWORD,
+		mongo_server: env.MONGO_SERVER,
+		mongo_name: env.MONGO_NAME,
+	}
 }
 
 export default dev;
