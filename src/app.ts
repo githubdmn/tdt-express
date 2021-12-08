fb14e10c3c90adac7dfb778cd214729d9489d873
Damjan Ivanovic <damjanivanovic@Damjans-MacBook-Pro.local>
2021-12-08 14:45:49 +0100

    eslint fix

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,10 @@
 
 import dev from './config'
-// eslint-disable-next-line
-import { mongo_connect } from './database'
+import { mongoConnect } from './database'
 import { express } from 'express'
 
 console.log(dev())
 
-// eslint-disable-next-line
-mongo_connect()
+mongoConnect()
   .then(() => console.log('successfully connected to mongodb'))
   .catch(() => console.log('unsuccessfully connected to mongodb'))
