bd01b92dac18b7320d07ac1d528ea835e897ce36
Damjan Ivanovic <damjanivanovic@Damjans-MacBook-Pro.local>
2021-12-08 14:18:15 +0100

    shows environment variables and connects to database

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,12 @@
 
-console.log('FIRST TEST')
+import dev from './config'
+import { dblink, mongo_connect } from './database'
+
+console.log(dev());
+console.log(dblink);
+
+mongo_connect()
+	.then(() => console.log('successfully connected to mongodb'))
+	.catch(() => console.log('unsuccessfully connected to mongodb'))
+
 
