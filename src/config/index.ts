bbf2cbf6152303693bff2a7afe7a5c0199118094
Damjan Ivanovic <damjanivanovic@Damjans-MacBook-Pro.local>
2021-12-08 10:57:24 +0100

    Adds env values into variable

---
+++ src/config/index.ts
@@ -0,0 +1,22 @@
+
+import {resolve} from 'path';
+import {env} from 'process';
+import {config} from 'dotenv';
+
+config({
+	path: resolve(__dirname, '../../.env'),
+});
+
+const dev = {
+	port: env.PORT,
+	salt: env.SALT_WORK_FACTOR,
+	access_token_secret: env.ACCESS_TOKEN_SECRET,
+	refresh_token_secret: env.REFRESH_TOKEN_SECRET,
+	mongo_username: env.MONGO_USERNAME,
+	mongo_password: env.MONGO_PASSWORD,
+	mongo_server: env.MONGO_SERVER,
+	mongo_name: env.MONGO_NAME,
+}
+
+export default dev;
+
