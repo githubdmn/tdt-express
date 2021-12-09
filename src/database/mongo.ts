56372c5cdf055ddccb7e358b81d6c12daea4cdbc
Damjan Ivanovic <damjanivanovic@Damjans-MacBook-Pro.local>
2021-12-09 16:40:46 +0100

    export connect/disconnect

--- src/database/mongo.ts
+++ src/database/mongo.ts
@@ -4,13 +4,10 @@ import dev from '../config'
 
 const dblink = `mongodb+srv://${dev().mongo_username}:${dev().mongo_password}@${dev().mongo_server}/${dev().mongo_name}`
 
-const mongoConnect = async () => await connect(dblink, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
+export const mongoConnect = async () => await connect(dblink, {
+  // useCreateIndex: true,
+  // useNewUrlParser: true,
+  // useUnifiedTopology: true
 })
 
-const mongoDisconnect = async () => await disconnect()
-
-module.exports.dblink = dblink
-module.exports.mongoConnect = mongoConnect
-module.exports.mongoDisconnect = mongoDisconnect
+export const mongoDisconnect = async () => await disconnect()
