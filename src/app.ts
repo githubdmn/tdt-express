e3394d4f261c7c92f4f25dbe5d6eaaf326d0bee2
Damjan Ivanovic <damjanivanovic@Damjans-MacBook-Pro.local>
2021-12-09 16:45:32 +0100

    connect to MongoDB and collection created

--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,24 @@
 
 import dev from './config'
 import { mongoConnect } from './database'
-import express, {Request, Response} from 'express'
+import express, { Request, Response } from 'express'
+import UserMongoose from './models/user.mongoose'
 
 mongoConnect()
-  .then(() => console.log('successfully connected to mongodb'))
-  .catch(() => console.log('unsuccessfully connected to mongodb'))
+  .then(() => {
+    console.log('Successfully connected to DB')
+    UserMongoose.createCollection()
+      .then(() => console.log('Succesfully to created the user collection'))
+      .catch(() => console.log('Failed to create the user collection'))
+  })
+  .catch(() => console.log('Failed to connect the DB'))
 
-const indexHeader = `<h3>21-12<h3></break>`;
-const indexMessage = `<p>Date posted 08/12/2021\nHome index - ${new Date()}</p>`;
+const indexHeader = '<h3>21-12<h3></break>'
+const indexMessage = `<p>Date posted 08/12/2021<br>Home index - ${new Date().toString()}</p>`
 
 express()
-	.use(express.json())
-	.use(express.urlencoded({ extended: true }))
-	.get('/', (req: Request, res:Response) => { res.status(200).send(indexHeader.concat(indexMessage)) })
-	.get('/health', (req: Request, res:Response) => { res.status(200).send('OK') })
-	.listen(dev().port, () => { console.log(`${new Date()}\nListening on port ${dev().port} ...`); });
+  .use(express.json())
+  .use(express.urlencoded({ extended: true }))
+  .get('/', (req: Request, res: Response) => { res.status(200).send(indexHeader.concat(indexMessage)) })
+  .get('/health', (req: Request, res: Response) => { res.status(200).send('OK') })
+  .listen(dev().port, () => { console.log(`${new Date().toString()}\nListening on port ${dev().port} ...`) })
