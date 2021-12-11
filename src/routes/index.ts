046fec9751d851c19eb9a9c80d9decaf64430676
Damjan Ivanovic <damjanivanovic@Damjans-MacBook-Pro.local>
2021-12-11 16:06:03 +0100

    user route controller added

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,6 +1,8 @@
 import { Router, Request, Response } from 'express'
+import userController from './user.controller'
 
 export default Router()
   .get('/health', (req: Request, res: Response) => {
     res.status(200).send('OK')
   })
+  .use('/user', userController)
