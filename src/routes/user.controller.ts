94c9ef8b0a351cddc84512ca45a854bb6f4fe69b
Damjan Ivanovic <damjanivanovic@Damjans-MacBook-Pro.local>
2021-12-13 12:26:11 +0100

    test user routes

--- src/routes/user.controller.ts
+++ src/routes/user.controller.ts
@@ -4,7 +4,19 @@ export default Router()
   .get('/:id', (req: Request, res: Response) => {
     res.status(200).send(`Get user with id ${req.params.id}`)
   })
-  .post('/register', (req: Request, res: Response) => {})
-  .put('/:id/update', (req: Request, res: Response) => { })
-  .patch('/:id/patch', (req: Request, res: Response) => { })
-  .delete('/:id/delete', (req: Request, res: Response) => { })
+  .post('/', (req: Request, res: Response) => {
+    console.log('user - register')
+    res.status(200).send('Register user')
+  })
+  .put('/:id', (req: Request, res: Response) => {
+    console.log('user - update put')
+    res.status(200).send(`Put user with id ${req.params.id}`)
+  })
+  .patch('/:id', (req: Request, res: Response) => {
+    console.log('user - update patch')
+    res.status(200).send(`Patch user with id ${req.params.id}`)
+  })
+  .delete('/:id', (req: Request, res: Response) => {
+    console.log('user - delete')
+    res.status(200).send(`Delete user with id ${req.params.id}`)
+  })
