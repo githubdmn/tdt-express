63eb580926168a528829e98a96fa11677cd55c98
githubdmn <dmninbox@gmail.com>
2022-01-17 22:23:12 +0100

    login validation refactored

--- src/middleware/user/login.ts
+++ src/middleware/user/login.ts
@@ -1,6 +1,6 @@
 /* eslint-disable no-unused-expressions */
 
-import { Request, Response, NextFunction } from 'express'
+import { Request } from 'express'
 import Joi from 'joi'
 
 const loginObject = Joi.object({
@@ -8,10 +8,13 @@ const loginObject = Joi.object({
   password: Joi.string().required()
 })
 
-const loginValidation = (req: Request, res: Response, next: NextFunction) => {
-  const valid = loginObject.validate(req.body)
-  if (valid.error) return valid.value
-  else next
+const loginValidation = (req: Request): Object => {
+  const valid: any = loginObject.validate(req.body)
+  const isError = Joi.isError(valid.error)
+  return {
+    error: isError,
+    message: isError ? valid.error.details[0].message : 'Data is valid'
+  }
 }
 
 export default loginValidation
