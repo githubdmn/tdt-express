4f14a11a2094819387fc7b3c199b5575d8b950d7
githubdmn <dmninbox@gmail.com>
2022-01-17 22:24:56 +0100

    user register controller refactored

--- src/services/user/user.login.ts
+++ src/services/user/user.login.ts
@@ -2,24 +2,27 @@
 import models from '../../models/'
 import { compare } from 'bcrypt'
 
-const login = async (input: any): Promise<string> => {
-  // const exists: Promise<any> | null = await models.UserMongoose.findOne({ email: input.email })
-  // console.log(exists)
-  // if (exists != null) {
-  //   input.id = exists.id
-  //   const result: boolean = await compare(input.password, exists.password)
-  //   if (result) {
-  //     input.createdAt = new Date().toString()
-  //     await models.LoginMongoose.create(input)
-  //     return 'User successfully registered'
-  //   }
-  //   else {
-  //     return 'Invalid username and password'
-  //   }
-  // } else {
-  //   return `User does not exist with email: ${input.email}`
-  // }
-  return `User does not exist with email: ${input.email}`
+const login = async (input: any): Promise<any> => {
+  const exists: any = await models.LoginMongoose.findOne({ email: input.email })
+  if (exists != null) {
+    // eslint-disable-next-line spaced-comment
+    const result: boolean = await compare(input.password, exists.password)
+    if (result) {
+      const saved = await models.LoginMongoose.findOneAndUpdate(exists.email,
+        {
+          lastLogin: new Date(),
+          userAgent: input.userAgent
+        })
+      return { ...saved, error: false }
+    } else {
+      return { message: 'Invalid credentials', error: true }
+    }
+  } else {
+    return {
+      message: `User does not exist with email: ${input.email}`,
+      error: true
+    }
+  }
 }
 
 export default login
