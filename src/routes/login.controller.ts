2955a15b5d99ce8ad2f0ff42cc9e28cadd412b24
githubdmn <dmninbox@gmail.com>
2022-01-17 22:24:26 +0100

    login  conroller

--- src/routes/login.controller.ts
+++ src/routes/login.controller.ts
@@ -1,14 +1,22 @@
 import { Router, Request, Response, NextFunction } from 'express'
 import services from '../services'
+import middleware from '../middleware'
+import utils from '../utils'
 
 export default Router()
   .post('/', async (req: Request, res: Response, next: NextFunction) => {
-    res.status(200).send(services.userLogin(req.body))
-    // middleware.loginValidation(req, res, next)
-    // try {
-    // const register = await service.userRegister(req.body)
-    // res.status(200).json(register)
-    // } catch (error) {
-    // res.status(200).json(error)
-    // }
+    const valid: any = middleware.loginValidation(req)
+    if (valid.error) return res.status(200).json(valid.message)
+    const login: Promise<any> | any = await services.userLogin({ ...req.body, userAgent: req.get('User-Agent') })
+    if (login.error) return res.status(200).json(login.message)
+    else {
+      const signObject: Object = {
+        username: login.email,
+        password: login.password,
+        login: login.id
+      }
+      const accessToken = utils.signUser(signObject, { expiresIn: '15m' })
+      const refreshToken = utils.signUser(signObject, { expiresIn: '1y' })
+      return res.status(200).json({ accessToken: accessToken, refreshToken: refreshToken })
+    }
   })
