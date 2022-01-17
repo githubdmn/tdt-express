e806fb13e206343f2773dd41131c88376ab6a2f0
githubdmn <dmninbox@gmail.com>
2022-01-17 22:25:48 +0100

    user register service refactored

--- src/services/user/user.register.ts
+++ src/services/user/user.register.ts
@@ -1,26 +1,28 @@
 
 import models from '../../models/'
-import { nanoid } from 'nanoid'
+import { customAlphabet } from 'nanoid'
 import { hashPassword } from './hash.password'
 
 const register = async (input: any): Promise<string> => {
   const exists = await models.UserMongoose.findOne({ email: input.email })
   if (exists == null) {
-    input.id = nanoid()
+    input.id = customAlphabet('1234567890', 6)()
     input.password = await hashPassword(input.password)
     input.createdAt = new Date().toString()
     input.updatedAt = new Date().toString()
-    await models.UserMongoose.create(input)
-    await models.LoginMongoose.create({
+    const register: any = await models.UserMongoose.create(input)
+    const login: any = await models.LoginMongoose.create({
       id: input.id,
       email: input.email,
       password: input.password,
       lastLogin: new Date().toString(),
-      lastLogout: new Date().toString()
+      lastLogout: new Date().toString(),
+      userAgent: input.userAgent
     })
-    return 'User successfully registered'
+    if (register.id && login.id) return 'User successfully registered'
+    else return 'An error occured while saving to the database'
   } else {
-    return `User exists with email: ${input.email}`
+    return `User exists with email: ${exists.email}`
   }
   // TODO: email verification
 }
