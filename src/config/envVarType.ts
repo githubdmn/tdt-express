87815a00e7c11f6546e331f2ff4136d33e8bdb20
githubdmn <dmninbox@gmail.com>
2022-01-17 22:22:50 +0100

    env var changed

--- src/config/envVarType.ts
+++ src/config/envVarType.ts
@@ -1,11 +1,10 @@
 /* eslint-disable no-tabs */
 export default interface EnvVar {
 	port: string | undefined,
-	salt: string | undefined,
 	// eslint-disable-next-line camelcase
-	access_token_secret: string | undefined,
+	publicKey: string | undefined,
 	// eslint-disable-next-line camelcase
-	refresh_token_secret: string | undefined,
+	privateKey: string | undefined,
 	// eslint-disable-next-line camelcase
 	mongo_username: string | undefined,
 	// eslint-disable-next-line camelcase
--- src/config/index.ts
+++ src/config/index.ts
@@ -5,7 +5,7 @@ import { config } from 'dotenv'
 import EnvVar from './envVarType'
 
 const environment = (envFlag: String[]): EnvVar => {
-  console.log(envFlag) // TODO: remove this line
+  // console.log(envFlag) // TODO: remove this line
   /* eslint-env node, mocha */
   // const envVar = process.argv[2] ? '.env.test' : '.env'
   const envVar = '.env'
@@ -15,9 +15,8 @@ const environment = (envFlag: String[]): EnvVar => {
 
   return {
     port: env.PORT,
-    salt: env.SALT_WORK_FACTOR,
-    access_token_secret: env.ACCESS_TOKEN_SECRET,
-    refresh_token_secret: env.REFRESH_TOKEN_SECRET,
+    publicKey: env.PUBLIC_KEY,
+    privateKey: env.PRIVATE_KEY,
     mongo_username: env.MONGO_USERNAME,
     mongo_password: env.MONGO_PASSWORD,
     mongo_server: env.MONGO_SERVER,
