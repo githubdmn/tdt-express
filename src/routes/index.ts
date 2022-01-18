48f40ae7b030e5ee71d92ece1f16d9038593983a
githubdmn <dmninbox@gmail.com>
2022-01-18 06:08:33 +0100

    TODO: auth middleware

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,13 @@
 import { Router, Request, Response } from 'express'
 import userController from './user.controller'
 import loginController from './login.controller'
+import registerController from './register.controller'
 
 export default Router()
   .get('/health', (req: Request, res: Response) => {
     res.status(200).send('OK')
   })
+  .use('/register', registerController)
   .use('/login', loginController)
+  // TODO: auth middleware
   .use('/user', userController)
